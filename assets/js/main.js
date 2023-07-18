a256ae50ead8218789603c42acb1d2747a2f7a01
Ghulam Mustafa <ghulammustafa9901@gmail.com>
2023-07-18 23:06:59 +0500

    navbar issue resolve 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,16 @@
 var navToggle = document.getElementById("nav-toggle");
 var offCanvas = document.getElementById("off-canvas");
+var wrapper = document.getElementById("wrapper");
 var bodyElement = document.body;
 
 navToggle.addEventListener("click", function () {
   if (navToggle.classList.contains("open")) {
     navToggle.classList.remove("open");
     offCanvas.classList.remove("off-canvas-click");
-    bodyElement.classList.remove("shadow1");
+    wrapper.classList.remove("wrapper");
   } else {
     navToggle.classList.add("open");
     offCanvas.classList.add("off-canvas-click");
-    bodyElement.classList.add("shadow1");
+    wrapper.classList.add("wrapper");
   }
 });
